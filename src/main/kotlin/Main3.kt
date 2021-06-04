e2762abce9b457f6a348428e6acc37c6f1cbdadb
mark.ng@auspost.com.au <markchristopherng@gmail.com>
2021-06-04 14:51:35 +1000

    add comments

--- src/main/kotlin/Main3.kt
+++ src/main/kotlin/Main3.kt
@@ -9,7 +9,7 @@ fun main() = runBlocking {
 suspend fun printHelloWorld(parentCoroutineContext: CoroutineContext) {
 
     //Coroutine scope =  CoroutineContext = Dispatcher (where to run coroutine) + Job (cancellable thing)
-    CoroutineScope(Dispatchers.Default + parentCoroutineContext.job).launch {
+    CoroutineScope(Dispatchers.Default + parentCoroutineContext.job).launch {   // if you use Job() the coroutine no longer belongs to the parent coroutine.
         log("starting coroutine")
         delay(1000) // non-blocking delay for 1 second
         log("delay coroutine")
